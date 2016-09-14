53fb1900c656583369d9f80cd3ec683557f47622
Pieter Breed <pieter.breed@gmail.com>
2016-09-14 13:15:47 +0200

    included a deployment script for datomic

---
+++ local-macosx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export HOUSTAN_IP=192.168.96.100
+export HOUSTAN_HOSTNAME=houstan.local
+export SSH_PUBLIC_KEY_PATH=$HOME/.ssh/id_rsa.pub
+export DIST_FOLDER=~/Downloads
+export DATOMIC_VERSION=0.9.5385
+
+
